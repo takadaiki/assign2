4b1531bfcad2b8869868254ab32dc56683198c92
EC2 Default User <ec2-user@ip-172-31-19-84.ec2.internal>
2022-12-09 23:29:29 +0000

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+       flash[:notice] = "投稿にsuccessfullyしました。"
+    else
+      @books = Book.all
+       flash[:alret] = "投稿にerrorしました。"
+      render :index
+
+    end
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+    redirect_to book_path(@book.id)
+    flash[:notice] = "更新にsuccessfullyしました。"
+    else
+      flash[:alret] = "更新にerrorしました。"
+      render :edit
+
+    end
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
